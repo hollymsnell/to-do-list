903aef3f2239599f8b08458b85cc343217c21ed0
hollymsnell <hollymsnell@gmail.com>
2023-12-17 19:03:40 -0700

    Add index action

--- db.py
+++ db.py
@@ -48,3 +48,13 @@ def initial_setup():
 
 if __name__ == "__main__":
     initial_setup()
+
+
+def todos_all():
+    conn = connect_to_db()
+    rows = conn.execute(
+        """
+        SELECT * FROM todos
+        """
+    ).fetchall()
+    return [dict(row) for row in rows]
